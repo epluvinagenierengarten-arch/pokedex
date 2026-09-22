b894304551fea12c24d7627b3454c1df713606e6
Octerry <taaniyu@gmail.com>
2026-09-22 17:27:04 +0200

    ON A UN DARKMODE git add .!

--- darkmode.js
+++ darkmode.js
@@ -1,20 +1,25 @@
 // ------ DARKMODE ------
-const toggle = document.getElementById('theme-toggle');
-const icon = toggle.querySelector('.icon');
-
+// Theme
 const savedTheme = localStorage.getItem('theme')
 window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
 
-applyTheme(savedTheme);
+// Toggle
+const toggle = document.getElementById('theme-toggle');
+let icon = null;
+if (toggle != null) icon = toggle.querySelector('.icon');
 
-toggle.addEventListener('click', () => {
-  const current = document.documentElement.getAttribute('data-theme');
-  const newTheme = current === 'dark' ? 'light' : 'dark';
-  applyTheme(newTheme);
-  localStorage.setItem('theme', newTheme);
-});
+applyTheme(savedTheme);
 
 function applyTheme(theme) {
     document.documentElement.setAttribute('data-theme', theme);
-    icon.textContent = theme === 'dark' ? '✹' : '⏾';
+    if (icon != null) icon.textContent = theme === 'dark' ? '⏾' : '✹';
+}
+
+if (toggle != null && icon != null) { // Parce que pokedex a pas le toggle
+  toggle.addEventListener('click', () => {
+    const current = document.documentElement.getAttribute('data-theme');
+    const newTheme = current === 'dark' ? 'light' : 'dark';
+    applyTheme(newTheme);
+    localStorage.setItem('theme', newTheme);
+  });
 }
